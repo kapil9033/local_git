a6ac586b7d6a7b94d0dadc1a8e91f3adad079b61
_bhayani9033 <kapilbhayani9033@gmail.com>
2019-02-05 17:32:06 +0100

    First Time Edit

---
+++ Hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+
+int main(){
+	printf("Hello_World From my new OS");
+	//I am editing file at 17:16, 05.02.2019
+return 0;
+}
---
+++ Hello_kapil.c
@@ -0,0 +1,7 @@
+# include < stdio.h>
+
+
+int main(){
+  printf("Hello_Kapil!, How Are You???");
+return 0;
+}
